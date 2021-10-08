0d11a5d53e7b689b54a26706f849dfd5f0a507e0
Aymen <aymenbouazra994@gmail.com>
2021-10-08 19:38:39 +0100

    Random cat with api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react";
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+    const CatApi="https://api.thecatapi.com/v1/images/search"
+    const [CatURL,setCat] = useState("")
+    function randomCat(){
+        fetch(CatApi)
+            .then((res)=>res.json())   
+            .then((cats)=>{ 
+                const cat = cats[0].url
+                setCat(cat)
+                }
+            )
+            .catch((error)=>
+                console.log('error: ',error)
+                )
+    }
+    return(
+        <div className="Container">
+            <h1 >Random Cat</h1>
+            <img src={CatURL} alt="Cat.."/>
+            <button onClick={randomCat}>Random cat change</button>
+        </div>
+    )
+
 }
 
 export default App;
